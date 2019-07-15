31c79481dd4a48ff41e14a1bc8c0230834bbb14c
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2019-07-16 00:01:27 +0530

    Final Commit

---
+++ cmake-build-debug/CMakeFiles/MinimumSwaps2Alternate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinimumSwaps2Alternate.dir/MinimumSwaps2Alternate.cpp.o"
+  "MinimumSwaps2Alternate.pdb"
+  "MinimumSwaps2Alternate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinimumSwaps2Alternate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
