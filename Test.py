a244e7d5ed697df199d46ae5b1c1266891e744a6
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2019-08-04 09:19:08 +0530

    Added Test file

---
+++ Test.py
@@ -0,0 +1,34 @@
+def getEmailThreads(emails):
+    # Write your code here
+    sender = []
+    reciever = []
+    mess = ""
+    my_list = []
+    for i in emails:
+        for j in i:
+            if len(my_list) == 2:
+                break
+            elif j != ",":
+                mess += j
+            else:
+                my_list.append(mess)
+                mess = ""
+        sender.append(my_list[0])
+        reciever.append(my_list[1])
+        my_list.clear()
+    my_dict = {}
+    for i in sender:
+        for j in reciever:
+            my_dict[i] = j
+            reciever.pop(0)
+            break
+    print(my_dict)
+
+
+emails_count = int(input().strip())
+emails = []
+for i in range(emails_count):
+    emails_item = input()
+    emails.append(emails_item)
+print(getEmailThreads(emails))
+# print(len(result))
