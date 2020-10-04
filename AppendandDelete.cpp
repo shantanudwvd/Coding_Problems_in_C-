45e65d10140413b6a8191685b79616312f21f5a5
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2020-10-04 23:03:00 +0530

    Modified Append And Delete Code

--- AppendandDelete.cpp
+++ AppendandDelete.cpp
@@ -16,36 +16,41 @@ int main() {
     getline(cin, s);
     string t;
     getline(cin, t);
-    for (char i : s) {
-        str.push_back(i);
-    }
-    it = str.begin();
-    for (char i : t) {
-        str1.push_back(i);
-    }
-    it1 = str1.begin();
-    int k;
-    cin >> k;
-    for (int i = 0; i < str1.size(); i++) {
-        if (str1.size() > str.size()) {
-            count = (str1.size() - str.size());
-            break;
-        } else if (str.at(i) == str1.at(i)) {
-            it++;
-            it1++;
-            if (distance(str1.begin(), it1) == str1.size()) {
+    if (s == t) {
+        cout << "Yes" << endl;
+    } else {
+
+        for (char i : s) {
+            str.push_back(i);
+        }
+        it = str.begin();
+        for (char i : t) {
+            str1.push_back(i);
+        }
+        it1 = str1.begin();
+        int k;
+        cin >> k;
+        for (int i = 0; i < str1.size(); i++) {
+            if (str1.size() > str.size()) {
+                count = (str1.size() - str.size());
+                break;
+            } else if (str.at(i) == str1.at(i)) {
+                it++;
+                it1++;
+                if (distance(str1.begin(), it1) == str1.size()) {
+                    count = count + distance(it, str.end());
+                    break;
+                }
+            } else if (str.at(i) != str1.at(i)) {
                 count = count + distance(it, str.end());
+                count = count + distance(it1, str1.end());
                 break;
             }
-        } else if (str.at(i) != str1.at(i)) {
-            count = count + distance(it, str.end());
-            count = count + distance(it1, str1.end());
-            break;
         }
-    }
-    if (count <= k) {
-        cout << "Yes";
-    } else {
-        cout << "No";
+        if (count <= k) {
+            cout << "Yes";
+        } else {
+            cout << "No";
+        }
     }
 }
